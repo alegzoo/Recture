11dca18fa1985cd7e2a77da3a38cd3e7eac1e56c
rmiscik <>
2023-04-01 20:28:59 +0200

    Add checkboxes to timetable setup dialog

---
+++ src/components/SquareCheckbox.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-checkbox :model-value="modelValue" @update:model-value="(val: boolean) => emit('update:modelValue', val)"  :class="modelValue?'selected':''" true-icon="mdi-check" :ripple="false" :label="label" hide-details/>
+</template>
+
+<style lang="scss" scoped>
+    @import "@/styles/constants.scss";
+
+    .v-checkbox {
+
+        :deep(.v-icon) {
+            border: solid 2px black;
+            box-shadow: none;
+            background-color: transparent;
+            opacity: 100%;
+            transition: box-shadow 0.1s ease-in-out;
+        }
+
+        :deep(.v-label) {
+            opacity: 100%;
+            font-weight: bold;
+        }
+
+        :deep(.v-icon::before) {
+            visibility: hidden;
+        }
+
+        :deep(.v-label) {
+            padding-left: 5px;
+        }
+
+    }
+
+    .v-checkbox.selected {
+
+        :deep(.v-icon) {
+            border: solid 2px black;
+            box-shadow: 3px 3px 0px black;
+            background-color: $recture-yellow;
+            transition: box-shadow 0.1s ease-in-out;
+        }
+
+        :deep(.v-icon::before) {
+            visibility: visible;
+        }
+
+    }
+</style>
+
+<script lang="ts" setup>
+    const props = defineProps<{
+        modelValue?: boolean,
+        label: string
+    }>();
+
+    const emit = defineEmits<{
+        (e: "update:modelValue", val: boolean): void
+    }>();
+</script>
--- src/components/TimetableSetupDialog.vue
+++ src/components/TimetableSetupDialog.vue
@@ -1,7 +1,7 @@
 <template>
     <v-dialog activator="parent" class="h-100 justify-center" width="450" scroll-strategy="block">
         <template v-slot:activator>
-                    <v-btn icon="" theme="light"/>
+            <v-btn icon="" theme="light"/>
         </template>
         <v-card class="setup-timetable-card">
                 <template v-slot:title>
@@ -16,25 +16,27 @@
                     </v-row>
 
                     <v-row no-gutters fluid>
-                        <v-col cols="6">
-                            <v-checkbox class="checkbox" :ripple="false" label="Monday"></v-checkbox>
+                        <v-col>
+                            <SquareCheckbox v-model="daysOfWeek[0]" label="Monday"/>
+                            <SquareCheckbox v-model="daysOfWeek[1]" label="Tuesday"/>
+                            <SquareCheckbox v-model="daysOfWeek[2]" label="Wednesday"/>
+                            <SquareCheckbox v-model="daysOfWeek[3]" label="Thursday"/>
+                            <SquareCheckbox v-model="daysOfWeek[4]" label="Friday"/>
                         </v-col>
 
-                        <v-divider vertical :thickness="2"></v-divider>
+                        <v-divider class="mx-5" vertical :thickness="2"></v-divider>
 
-                        <v-col cols="4" class="pl-7">
-                            <v-checkbox :ripple="false" label="Saturday"></v-checkbox>
+                        <v-col>
+                            <SquareCheckbox v-model="daysOfWeek[5]" label="Saturday"/>
+                            <SquareCheckbox v-model="daysOfWeek[6]" label="Sunday"/>
                         </v-col>
-
                     </v-row>
 
-
                     <v-row no-gutters class="pt-3">
-                        <v-col cols="10" class="days-headline pl-1">
+                        <v-col class="days-headline pl-1">
                             <h4>Number of lessons in a day at your school</h4>
                         </v-col>
                     </v-row>
-
                 </v-card-text>
 
                 <v-row class="pt-4" height="auto" no-gutters>
@@ -46,7 +48,20 @@
 </v-dialog>
 </template>
 
+<style lang="scss" scoped>
+    .v-divider {
+        opacity: 100%;
+    }
+</style>
 
 <script lang="ts" setup>
+    import { ref, reactive, watch } from 'vue';
+    import SquareCheckbox from './SquareCheckbox.vue';
 
+    const daysOfWeek = reactive<boolean[]>([true, true, true, true, true, false, false]);
+    const start = ref<number | undefined>(undefined);
+
+    watch(start, () => {
+        console.log(start);
+    });
 </script>
